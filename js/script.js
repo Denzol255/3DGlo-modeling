75490b8d5bcd035ed41fd0c1cb6ce4dc648db57d
Denzol255 <den-zoloto@mail.ru>
2021-01-29 16:07:27 +0300

    Assigning a value to variables

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ let money, income, addExpenses, deposit, mission, period;
 alert("Hello world!");
 
 console.log("Hello, friends!");
+
+money = 1000;
+income = "investments";
+addExpenses = "internet, taxi, shopping";
+deposit = true;
+mission = 100000;
+period = 6;
