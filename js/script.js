3eaa6f05a5b5fdac41d5881404baaa5e36de5dc9
Denzol255 <den-zoloto@mail.ru>
2021-01-29 17:05:39 +0300

    Done second part of task

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
+"use strict";
+
 let money, income, addExpenses, deposit, mission, period;
 
 alert("Hello world!");
 
 console.log("Hello, friends!");
 
-money = 1000;
+money = 900;
 income = "investments";
-addExpenses = "internet, taxi, shopping";
+addExpenses = "Internet, Taxi, Shopping";
 deposit = true;
-mission = 100000;
+mission = 90000;
 period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+console.log(addExpenses.toLowerCase().split(", "));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
