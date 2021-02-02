f18a55107d23bbd473d3a2f4d3ac05e4b1358cff
Denzol255 <den-zoloto@mail.ru>
2021-02-02 09:59:13 +0300

    Fix: change position of strings

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 "use strict";
 
-let money = 900,
-  addExpenses = "Интернет, такси, одежда",
-  deposit = true,
-  budgetDay = money / 30;
 const income = "Инвестиции",
   mission = 90000,
   period = 6;
+let money = Number(prompt("Ваш месячный доход?")),
+  addExpenses = prompt(
+    "Перечислите возможные расходы за рассчитываемый период через запятую"
+  ),
+  deposit = confirm("Есть ли у вас депозит в банке?"),
+  expenses1 = prompt("Введите обязательную статью расходов?"),
+  amount1 = +prompt("Во сколько это обойдется?"),
+  expenses2 = prompt("Введите обязательную статью расходов?"),
+  amount2 = +prompt("Во сколько это обойдется?"),
+  budgetMonth = money - amount1 - amount2,
+  monthsForMisson = Math.round(mission / budgetMonth),
+  budgetDay = Math.floor(budgetMonth / 30);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -15,24 +23,6 @@ console.log(addExpenses.length);
 console.log(`Период равен ${period} месяцев`);
 console.log(`Цель заработать ${mission} долларов`);
 console.log(addExpenses.toLowerCase().split(", "));
-console.log(budgetDay);
-
-money = Number(prompt("Ваш месячный доход?"));
-
-addExpenses = prompt(
-  "Перечислите возможные расходы за рассчитываемый период через запятую"
-);
-
-deposit = confirm("Есть ли у вас депозит в банке?");
-
-let expenses1 = prompt("Введите обязательную статью расходов?"),
-  amount1 = Number(prompt("Во сколько это обойдется?")),
-  expenses2 = prompt("Введите обязательную статью расходов?"),
-  amount2 = Number(prompt("Во сколько это обойдется?")),
-  budgetMonth = money - amount1 - amount2,
-  monthsForMisson = Math.round(mission / budgetMonth);
-
-budgetDay = Math.floor(budgetMonth / 30);
 console.log("Бюджет на месяц:", budgetMonth, "долларов");
 console.log(`Цель будет достигнута за ${monthsForMisson} месяцев (-а)`);
 console.log(`Бюджет на день: ${budgetDay}`);
