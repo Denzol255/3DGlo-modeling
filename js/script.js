f8ced4d935d4625320d86256c0074b219058d131
Denzol255 <den-zoloto@mail.ru>
2021-02-05 13:25:20 +0300

    Create: part of basic lesson06

--- js/script.js
+++ js/script.js
@@ -5,75 +5,79 @@ let isNumber = function (n) {
 
 let money;
 
-const income = "Инвестиции",
-  mission = 90000,
-  period = 6,
-  start = function () {
-    do {
-      money = prompt("Ваш месячный доход?");
-    } while (!isNumber(money));
-  };
+const start = function () {
+  do {
+    money = prompt("Ваш месячный доход?");
+  } while (!isNumber(money));
+};
 start();
 
-let addExpenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую",
-    "Драгоценности, одежда, бензин"
-  ),
-  deposit = confirm("Есть ли у вас депозит в банке?"),
-  expenses = [],
-  showTypeOf = function (data) {
-    console.log(data, typeof data);
-  },
-  getExpensesMonth = function () {
-    let sum = 0,
-      temp = 0;
+let appData = {
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  deposit: false,
+  mission: 90000,
+  period: 6,
+  asking: function () {
+    let addExpenses = prompt(
+      "Перечислите возможные расходы за рассчитываемый период через запятую",
+      "Драгоценности, одежда, бензин"
+    );
+    appData.addExpenses = addExpenses.toLowerCase().split(", ");
+    appData.deposit = confirm("Есть ли у вас депозит в банке?");
     for (let i = 0; i < 2; i++) {
-      expenses[i] = prompt("Введите обязательную статью расходов?");
-      do {
-        temp = prompt("Во сколько это обойдется?");
-      } while (!isNumber(temp));
-      sum += +temp;
+      let key = prompt("Введите обязательную статью расходов?");
+      appData.expenses[key] = Number(prompt("Во сколько это обойдется?")); // Добавить проверку на число!
     }
-    console.log(expenses);
+    console.log(appData.expenses);
+  },
+  budget: money,
+  budgetDay: 0,
+  budgetMonth: 0,
+  expensesMonth: 0,
+  getExpensesMonth: function () {
+    let sum = 0;
+    for (let key in appData.expenses) {
+      sum += appData.expenses[key];
+    }
     return sum;
   },
-  expensesAmount = getExpensesMonth(),
-  getAccumulatedMonth = function () {
-    return money - expensesAmount;
+  getAccumulatedMonth: function (cash, exps) {
+    return cash - exps;
   },
-  accumulatedMonth = getAccumulatedMonth(),
-  getTargetMonth = function () {
-    let target = Math.round(mission / accumulatedMonth);
+  getTargetMonth: function (mis, acc) {
+    let target = Math.round(mis / acc);
     if (target > 0 && isFinite(target)) {
       console.log(`Цель будет достигнута за ${target} месяцев (-а)`);
     } else {
       console.log("Цель не будет достигнута");
     }
   },
-  targetMonth = getTargetMonth(),
-  budgetDay = Math.floor(accumulatedMonth / 30),
-  getStatusIncome = function () {
-    if (budgetDay >= 1200) {
+  getStatusIncome: function () {
+    if (appData.budgetDay >= 1200) {
       console.log("У вас высокий уровень дохода");
-    } else if (budgetDay >= 600 && budgetDay < 1200) {
+    } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
       console.log("У вас средний уровень дохода");
-    } else if (budgetDay < 600 && budgetDay > 0) {
+    } else if (appData.budgetDay < 600 && appData.budgetDay > 0) {
       console.log("У вас низкий уровень дохода");
-    } else if (budgetDay === 0) {
+    } else if (appData.budgetDay === 0) {
       console.log("У вас нет доходов");
     } else {
       console.log("Что-то пошло не так");
     }
-  };
+  },
+};
 
-money = Number(money);
-/* Иначе money отображается как string, что думаю не совсем правильно, если преобразовывать сразу, то пустая строка 
-перобразуется в 0 */
+appData.asking();
 
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
+let expensesAmount = appData.getExpensesMonth(),
+  accumulatedMonth = appData.getAccumulatedMonth(money, expensesAmount),
+  targetMonth = appData.getTargetMonth(appData.mission, accumulatedMonth);
+
 console.log("Расходы за месяц: " + expensesAmount);
-console.log(addExpenses.toLowerCase().split(", "));
-console.log(`Бюджет на день: ${budgetDay}`);
-getStatusIncome();
+console.log(`Бюджет на день: ${appData.budgetDay}`);
+appData.getStatusIncome();
+
+// Math.floor(appData.getAccumulatedMonth / 30);
