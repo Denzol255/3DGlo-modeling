6228e5718336f5885060df4cb6d6c0d2d73103e6
Denzol255 <den-zoloto@mail.ru>
2021-02-09 18:00:14 +0300

    Create: full basic task of lesson08

--- js/script.js
+++ js/script.js
@@ -3,11 +3,15 @@ let isNumber = function (n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+let isString = function (value) {
+  return isNaN(value);
+};
+
 let money;
 
 const start = function () {
   do {
-    money = prompt("Ваш месячный доход?");
+    money = prompt("Ваш месячный доход?", 50000);
   } while (!isNumber(money));
 };
 start();
@@ -18,17 +22,39 @@ let appData = {
   expenses: {},
   addExpenses: [],
   deposit: false,
+  percentDeposit: 0,
+  moneyDeposit: 0,
   mission: 90000,
   period: 6,
   asking: function () {
-    let addExpenses = prompt(
-      "Перечислите возможные расходы за рассчитываемый период через запятую",
-      "Драгоценности, одежда, бензин"
-    );
-    appData.addExpenses = addExpenses.toLowerCase().split(", ");
+    if (confirm("Есть ли у вас дополнительный источник заработка?")) {
+      let itemIncome, cashIncome;
+      do {
+        itemIncome = prompt(
+          "Какой у вас дополнительный источник дохода?",
+          "Инвестиции"
+        );
+      } while (!isString(itemIncome));
+      do {
+        cashIncome = prompt("Сколько в месяц вы зарабатываете на этом?", 10000);
+      } while (!isNumber(cashIncome));
+      appData.income[itemIncome] = cashIncome;
+    }
+
+    do {
+      appData.addExpenses = prompt(
+        "Перечислите возможные расходы за рассчитываемый период через запятую",
+        "     Интернет , комуналка ,такси,аптека, детский сад "
+      );
+    } while (!isString(appData.addExpenses));
+    appData.addExpenses = appData.addExpenses.trim();
+    appData.addExpenses = appData.addExpenses.toLowerCase().split(",");
     appData.deposit = confirm("Есть ли у вас депозит в банке?");
     for (let i = 0; i < 2; i++) {
-      let key = prompt("Введите обязательную статью расходов?");
+      let key;
+      do {
+        key = prompt("Введите обязательную статью расходов?");
+      } while (!isString(key));
       do {
         appData.expenses[key] = prompt("Во сколько это обойдется?");
       } while (!isNumber(appData.expenses[key]));
@@ -40,7 +66,16 @@ let appData = {
   budgetDay: 0,
   budgetMonth: 0,
   expensesMonth: 0,
-  //Метод возвращает все расходы
+  /* Метод убирает у каждого элемента массива пробелы в начале и конце, вырезает первый символ, делает его заглавным и 
+  соединяет с тем же элементом без первого символа и без пробелов в начале и конце
+  */
+  getExpenses: function () {
+    appData.addExpenses = appData.addExpenses.map(
+      (item) => item.trim().slice(0, 1).toUpperCase() + item.trim().slice(1)
+    );
+    console.log(appData.addExpenses.join(", "));
+  },
+  // Метод возвращает все расходы
   getExpensesMonth: function () {
     for (let key in appData.expenses) {
       appData.expensesMonth += appData.expenses[key];
@@ -48,7 +83,7 @@ let appData = {
     return appData.expensesMonth;
   },
 
-  //Метод возвращает доходы за месяц (Доходы минус расходы) + бюджет на день
+  // Метод возвращает доходы за месяц (Доходы минус расходы) + бюджет на день
   getBudget: function () {
     appData.budgetMonth = appData.budget - appData.expensesMonth;
     appData.budgetDay = Math.floor(appData.budgetMonth / 30);
@@ -75,14 +110,30 @@ let appData = {
       console.log("Что-то пошло не так");
     }
   },
+  getInfoDeposit: function () {
+    if (appData.deposit) {
+      do {
+        appData.percentDeposit = prompt("Какой годовой процент?", 10);
+      } while (!isNumber(appData.percentDeposit));
+      do {
+        appData.moneyDeposit = prompt("Какая сумма заложена?", 10000);
+      } while (!isNumber(appData.moneyDeposit));
+    }
+  },
+  calcSavedMoney: function () {
+    return appData.budgetMonth * appData.period;
+  },
 };
 
 appData.asking();
+appData.getExpenses();
 appData.getExpensesMonth();
 appData.getBudget();
 console.log("Расходы за месяц: " + appData.expensesMonth);
 appData.getTargetMonth();
 appData.getStatusIncome();
+appData.getInfoDeposit();
+console.log(appData.calcSavedMoney());
 
 for (let key in appData) {
   console.log(
